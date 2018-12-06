e1c08525fc4970e4de6497e7f239682e9de57ce1
email <email>
2018-12-06 10:41:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a805_02/mymultimemoapk/OnColorSelectedListener.java
@@ -0,0 +1,17 @@
+package com.example.a805_02.mymultimemoapk;
+
+/**
+ * 색상 선택 리스너
+ *
+ * @author Mike
+ */
+public interface OnColorSelectedListener {
+
+	/**
+	 * Called when a color is selected
+	 *
+	 * @param color
+	 */
+	public void onColorSelected(int color);
+
+}
